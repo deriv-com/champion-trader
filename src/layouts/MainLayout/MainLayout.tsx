49db678a2c1057c6664d57476f8d96bb2fff47f6
Farabi <farabi@deriv.com>
2025-02-16 23:50:21 +0800

    fix: desktop and responsive layout

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -12,7 +12,7 @@ interface MainLayoutProps {
 }
 
 export const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
-  const { isMobile } = useDeviceDetection();
+  const { isMobile, isDesktop } = useDeviceDetection();
   const { isLandscape, setIsLandscape } = useOrientationStore();
   const [isSidebarOpen, setSidebarOpen] = React.useState(false);
   const [isMenuOpen, setMenuOpen] = React.useState(false);
@@ -36,10 +36,11 @@ export const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   }, [isMobile, isLandscape]);
 
   return (
-    <div className="flex min-h-[100dvh] h-[100dvh] relative">
-      <SideNav isSidebarOpen={isSidebarOpen} setSidebarOpen={setSidebarOpen} setMenuOpen={setMenuOpen} isMenuOpen={isMenuOpen} />
-      <div className={`flex flex-col ${isSidebarOpen || isMenuOpen ? "w-[75%] ml-auto" : "w-full"} overflow-hidden transition-all duration-300`}>
-        <Header className="sticky top-0 z-50 w-full" />
+    <div className="min-h-[100dvh] h-[100dvh] flex flex-col">
+      <Header className="sticky top-0 z-50 w-full" />
+      <div className={`flex flex-1 relative ${isDesktop ? "overflow-hidden" : "" }`}>
+        <SideNav isSidebarOpen={isSidebarOpen} setSidebarOpen={setSidebarOpen} setMenuOpen={setMenuOpen} isMenuOpen={isMenuOpen} />
+        <div className={`flex flex-col ${isSidebarOpen || isMenuOpen ? "w-[75%] ml-auto" : "w-full"} overflow-hidden transition-all duration-300`}>
         <div className={`${isSidebarOpen ? "w-[25%] flex-grow" : "hidden"} transition-all duration-300`}>
           <PositionsSidebar isOpen={isSidebarOpen} onClose={() => setSidebarOpen(false)} />
         </div>
@@ -48,6 +49,7 @@ export const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
           {children}
         </main>
         {!isLandscape && <Footer className="sticky bottom-0 z-50" />}
+      </div>
       </div>
     </div>
   );
