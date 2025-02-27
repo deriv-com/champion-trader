43de379df14d602cf20f21f7504338897a21dd3b
Farabi <farabi@deriv.com>
2025-02-27 16:12:52 +0800

    fix: test case

--- src/screens/TradePage/__tests__/TradePage.test.tsx
+++ src/screens/TradePage/__tests__/TradePage.test.tsx
@@ -196,7 +196,7 @@ describe('TradePage', () => {
     expect(tradePage.className).toContain('flex-row');
     expect(tradePage.className).toContain('relative');
     expect(tradePage.className).toContain('flex-1');
-    expect(tradePage.className).toContain('h-[calc(100vh-4rem)]');
+    expect(tradePage.className).toContain('flex flex-row relative h-full flex-1');
   });
 
   it('shows "Select Market" when no market is selected', () => {
