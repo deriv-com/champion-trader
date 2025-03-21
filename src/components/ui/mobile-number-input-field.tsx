e0e2c23eb33741ed7e80a0260c414c5ce61ee69c
Farabi <farabi@deriv.com>
2025-03-21 13:21:43 +0800

    fix: minor ui fixes

--- src/components/ui/mobile-number-input-field.tsx
+++ src/components/ui/mobile-number-input-field.tsx
@@ -67,7 +67,7 @@ export const MobileNumberInputField = React.forwardRef<
                         )}
                         <input
                             ref={ref}
-                            className="w-full bg-transparent focus:outline-none text-center font-ibm text-[1.125rem] font-normal leading-7 text-theme pr-8"
+                            className="w-full bg-transparent focus:outline-none text-center font-ibm text-[1.125rem] font-normal leading-7 text-theme pr-10"
                             inputMode="decimal"
                             onFocus={handleFocus}
                             onBlur={handleBlur}
--- src/screens/TradePage/components/TradeFormController.tsx
+++ src/screens/TradePage/components/TradeFormController.tsx
@@ -709,7 +709,7 @@ export const TradeFormController: React.FC<TradeFormControllerProps> = ({ isLand
                         </div>
                     </div>
 
-                    <div className="flex flex-1 mb-4 gap-2" id="trade-buttons">
+                    <div className="flex flex-1 mb-3 gap-2" id="trade-buttons">
                         {config.buttons.map((button) => (
                             <Suspense key={button.actionName} fallback={<div>Loading...</div>}>
                                 <TradeButton
